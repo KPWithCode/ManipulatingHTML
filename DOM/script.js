8a652051d64f1b594d092c32993b7130e23b8114
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-25 00:30:09 -0500

    Button produces an alert when clicked

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(event)   {
+let btn = document.createElement('button')
+btn.className ='button1'
+let btnText = document.createTextNode('button')
+btn.appendChild(btnText)
+document.body.appendChild(btn)
+
+btn.addEventListener('click', function() {
+   alert('Hello Sir Good Day')
+});
 
 
 
