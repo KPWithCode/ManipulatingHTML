a8548780dcdae946a988655cfcf8b6723c9924e6
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-25 21:35:23 -0500

    Added paragraph that switches to random color when clicked

--- DOM/script.js
+++ DOM/script.js
@@ -1,40 +1,46 @@
-document.addEventListener('DOMContentLoaded', function(event)   {
-let btn = document.createElement('button')
-btn.className ='button1'
-let btnText = document.createTextNode('DomButton')
-btn.appendChild(btnText)
-document.body.appendChild(btn)
+document.addEventListener('DOMContentLoaded', function (event) {
+    let btn = document.createElement('button')
+    btn.className = 'button1'
+    let btnText = document.createTextNode('DomButton')
+    btn.appendChild(btnText)
+    document.body.appendChild(btn)
 
-btn.addEventListener('click', function() {
-   alert('Hello Sir Good Day')
-});
-
-
+    btn.addEventListener('click', function () {
+        alert('Hello Sir Good Day')
+    });
 
 
+    //When button clicked alert value of input text box
+    let htmlButton = document.getElementById('htmlButton');
+    htmlButton.addEventListener('click', function () {
+
+        alert(document.getElementById('textBox').value);
 
 
-// let txtValue = document.createTextNode('The Value is' + textBox);
-// txtValue.appendTo(textBox)
-let htmlButton = document.getElementById('htmlButton');
-htmlButton.addEventListener('click', function() {
- 
-    alert(document.getElementById('textBox').value);
-    
+    });
 
-});
+    //make the div change to a different background color when your mouse hovers over it.
+    let colorDiv = document.getElementById('div1')
+    colorDiv.addEventListener('mouseenter', function () {
+        colorDiv.style.backgroundColor = 'red';
+    })
 
-//Set color of div
-let colorDiv = document.getElementById('div1')
-colorDiv.addEventListener('mouseenter', function()  {
-    colorDiv.style.backgroundColor = 'red';
+    colorDiv.addEventListener('mouseleave', function () {
+        colorDiv.style.backgroundColor = 'white';
+
+    });
+
+    let p = document.createElement('p')
+    let pTxt = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor purus non enim praesent elementum facilisis. ')
+    p.appendChild(pTxt)
+    document.body.appendChild(p)
+// Function for paragraph changing color when clicked
+p.addEventListener('click', function()  {
+    let colorChange = ['Green', 'Pink', 'Yellow', 'blue', 'black', 'brown', 'purple', 'Red'];
+    let randomIndex = Math.floor(Math.random() * colorChange.length)
+p.style.color = colorChange[randomIndex];
+p.style.cursor = 'pointer';
+
 })
-
-colorDiv.addEventListener('mouseleave', function()   {
-colorDiv.style.backgroundColor = 'white';
-
-});
-//When hover div changes 
-
-// End of DOM Content Loaded tag
+    // End of DOM Content Loaded tag
 });
