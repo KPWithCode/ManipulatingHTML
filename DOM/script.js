c5f04b06a3171fab27e87fcf0c016b9804ed2562
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-25 20:35:57 -0500

    When button is clicked it displays an alert with the message that is typed in the text box

--- DOM/script.js
+++ DOM/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(event)   {
 let btn = document.createElement('button')
 btn.className ='button1'
-let btnText = document.createTextNode('button')
+let btnText = document.createTextNode('DomButton')
 btn.appendChild(btnText)
 document.body.appendChild(btn)
 
@@ -14,8 +14,15 @@ btn.addEventListener('click', function() {
 
 
 
+// let txtValue = document.createTextNode('The Value is' + textBox);
+// txtValue.appendTo(textBox)
+let htmlButton = document.getElementById('htmlButton');
+htmlButton.addEventListener('click', function() {
+ 
+    alert(document.getElementById('textBox').value);
+    
 
-
+});
 
 // End of DOM Content Loaded tag
 });
