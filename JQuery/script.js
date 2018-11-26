fa027ee35eb74619ba9d86e8a37c62696f077ae4
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-25 23:56:48 -0500

    New button alerts whatever message is typed in the text box

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function (){
 
-let $button = $('<button>Button</Button>');
+let $button = $('<button>Friendly Button</Button>');
 $('body').append($button)
+$button.click(function()    {
+    alert('Hello, Good Day')
+});
 
-
-
+//When button clicked alert value of input text box
+let htmlButton = $('#htmlButton')
+htmlButton.click(function() {
+    alert($('#textBox').val())
+});
 
 // Dom Content Loaded end tag
 });
