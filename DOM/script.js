9c2d30d89e99062bccf3ace21bb4288e44cc0d32
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-25 22:52:57 -0500

    Each time a button is clicked a friend's name is added as a list item to the unordered list on the page

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (event) {
+
+    // Make button alert pleasant message
     let btn = document.createElement('button')
     btn.className = 'button1'
     let btnText = document.createTextNode('DomButton')
@@ -30,28 +32,45 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
     });
 
+    // Function for paragraph changing color when clicked
     let p = document.createElement('p')
     let pTxt = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor purus non enim praesent elementum facilisis. ')
     p.appendChild(pTxt)
     document.body.appendChild(p)
-// Function for paragraph changing color when clicked
-p.addEventListener('click', function()  {
-    let colorChange = ['Green', 'Pink', 'Yellow', 'blue', 'black', 'brown', 'purple', 'Red'];
-    let randomIndex = Math.floor(Math.random() * colorChange.length)
-p.style.color = colorChange[randomIndex];
-p.style.cursor = 'pointer';
 
-});
-let emptyDiv = document.getElementById('emptyDiv')
-let emptyBtn = document.getElementById('emptyDivBtn')
-emptyBtn.addEventListener('click', function()   {
-    let newSpan = document.createElement('span')
-    emptyDiv.appendChild(newSpan)
-    let newSpanTxt = document.createTextNode('Kyle')
-    newSpan.appendChild(newSpanTxt)
-});
-let friends = ['Barbera', 'Tesfa', 'Marsha', 'Marvin', 'Alex', 'Asim', 'Rachel', 'Andy', 'Michael', 'Ed']
-let lastBtn = document.getElementById('lastBtn');
+    p.addEventListener('click', function () {
+        let colorChange = ['Green', 'Pink', 'Yellow', 'blue', 'black', 'brown', 'purple', 'Red'];
+        let randomIndex = Math.floor(Math.random() * colorChange.length)
+        p.style.color = colorChange[randomIndex];
+        p.style.cursor = 'pointer';
+
+    });
+
+    // When button clicked span with my name is added to div
+    let emptyDiv = document.getElementById('emptyDiv')
+    let emptyBtn = document.getElementById('emptyDivBtn')
+    emptyBtn.addEventListener('click', function () {
+        let newSpan = document.createElement('span')
+        emptyDiv.appendChild(newSpan)
+        let newSpanTxt = document.createTextNode('Kyle')
+        newSpan.appendChild(newSpanTxt)
+    });
+
+    // When button is clicked friends name is added to list 
+    let friends = ['Barbera', 'Tesfa', 'Marsha', 'Marvin', 'Alex', 'Asim', 'Rachel', 'Andy', 'Michael', 'Ed']
+    let lastBtn = document.getElementById('lastBtn');
+    let ul = document.getElementById('ul');
+
+    lastBtn.addEventListener('click', function () {
+        let randomFriend = Math.floor(Math.random() * friends.length)
+        for (let i = 0; i < 1; i++) {
+            let li = document.createElement('li');
+            let friendNames = document.createTextNode(friends[randomFriend])
+            li.appendChild(friendNames)
+            ul.appendChild(li);
+        }
+
+    });
 
     // End of DOM Content Loaded tag
 });
