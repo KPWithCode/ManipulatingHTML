07f69c1cb965c59cf728fd0411b63c96bde26b37
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-26 00:08:12 -0500

    div changes to a different background when mouseovered

--- JQuery/script.js
+++ JQuery/script.js
@@ -12,5 +12,14 @@ htmlButton.click(function() {
     alert($('#textBox').val())
 });
 
+//make the div change to a different background color when your mouse hovers over it.
+let $colorDiv = $('#div1')
+$colorDiv.mouseenter(function() {
+   $(this).css('backgroundColor', 'red')
+});
+$colorDiv.mouseleave(function() {
+    $(this).css('backgroundColor', 'white')
+
+})
 // Dom Content Loaded end tag
 });
